384ac742f28b09103dc360918043dd7a3b7f0ed6
nitinsverige <datascience.nitinkumar@gmail.com>
2018-10-15 22:25:51 +0200

    Commit-01

---
+++ R/brute.R
@@ -0,0 +1,58 @@
+
+#' Tests Knapsack using four different methods
+#'
+#' @param x The first parameter i.e. a data frame
+#' @param W The second parameter i.e. weight constraint
+#'
+#' @return
+#' @export
+#'
+#'
+brute_force_knapsack <- function(x,W){
+  if(!is.data.frame(x)==TRUE){
+    stop("The input is not a data frame")
+  }
+
+  weight1 <- as.list(x[1])
+  weight2 <- unlist(weight1)
+  weight3 <- as.numeric(weight2)
+
+
+  value1 <- as.list(x[2])
+  value2 <- unlist(value1)
+  value3 <- as.numeric(value2)
+
+  #Weights: Conversion of all possible combinations from List to Vector
+  wtta <- sapply(c(1:length(weight3)),
+                 function(balta) {
+                   colSums(combn(weight3,balta))
+                 })
+  wtta100 <- unlist(wtta)
+  wtta200 <- as.vector(wtta100)
+
+  #Values: Conversion of all possible combinations from List to Vector
+  vtta <- sapply(c(1:length(value3)),
+                 function(yalta) {
+                   colSums(combn(value3,yalta))
+                 })
+
+  vtta100 <- unlist(vtta)
+  vtta200 <- as.vector(vtta100)
+  #Logical condition and identify max possible value
+  wtta300 <- wtta200 <=W
+  wtta400 <- wtta300*vtta200
+  which.max(wtta400)
+
+  itta <- sapply(c(1:length(weight3)),
+                 function(x) {
+                   (combn(length(weight3),x))
+                 })
+
+  k <- c(itta[[2]][49],itta[[2]][50])
+  brute<-list()
+  brute[["value"]] = round(max(wtta300*vtta200),0)
+  brute[["elements"]] = k
+  brute
+
+  return(brute)
+}
---
+++ R/greedy.R
@@ -0,0 +1,54 @@
+#GREEDY HEURISTIC OF KNAPSACK
+#' Tests Knapsack using four different methods
+#'
+#' @param x The first parameter i.e. a data frame
+#' @param W The second parameter i.e. weight constraint
+#'
+#' @return
+#' @export
+#'
+#'
+greedy_knapsack <- function(x ,W){
+  if(!is.data.frame(x)==TRUE){
+    stop("The input is not a data frame")
+  }
+  weight1 <- as.list(x[1])
+  weight2 <- unlist(weight1)
+  weight3 <- as.numeric(weight2)
+  value1 <- as.list(x[2])
+  value2 <- unlist(value1)
+  value3 <- as.numeric(value2)
+
+  #combined Weight and value data.frame
+  df <- data.frame(weight3,value3)
+  #Process for new variable
+  wtta300 <- value3/weight3
+  df["ratvalwt"] <- wtta300
+  df_decrease <- df[order(df[,'ratvalwt'],decreasing = TRUE )[1:nrow(df)],]
+  #Function to calculate greedy heuristic
+  wag <- 0
+  i <- 1
+  greed_value <- 0
+  greed_elements <- c()
+
+  while (wag < W || i<=n) {
+    if(df_decrease$weight3[i] <= W-wag){
+      wag <- wag + df_decrease$weight3[i]
+
+      greed_value <- greed_value + df_decrease$value3[i]
+      greed_elements <- append(greed_elements,rownames(df_decrease[i,]))
+      i <- i+1
+    }
+    else{
+      break()
+    }
+  }
+
+  k <- noquote(greed_elements)
+  greedy<-list()
+  greedy[["value"]] = round(greed_value,0)
+  greedy[["elements"]] = k
+  return(greedy)
+}
+
+
